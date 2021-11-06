75ed1aed349cf28bc7e06755ab880bf4a9378a3f
AbdulghaniAlhazmi <Abdulghani.Hassan.jed.k2@tuwaiq.edu.sa>
2021-11-06 19:43:34 +0300

    add page xml

--- app/src/main/java/com/example/todo/TodoFragment.kt
+++ app/src/main/java/com/example/todo/TodoFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import com.example.todo.Database.Todo
 
@@ -13,6 +15,12 @@ class TodoFragment : Fragment() {
 
     private lateinit var todo: Todo
     private lateinit var titleText : EditText
+    private lateinit var startDate : Button
+    private lateinit var endDate : Button
+    private lateinit var extraInfo : EditText
+    private lateinit var extraInfoBox : CheckBox
+    private lateinit var isDone : CheckBox
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,11 +33,27 @@ class TodoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_todo, container, false)
-
+        titleText = view.findViewById(R.id.todo_title)
+        startDate = view.findViewById(R.id.start_date)
+        endDate = view.findViewById(R.id.end_date)
+        extraInfo = view.findViewById(R.id.extra_infoText)
+        extraInfoBox = view.findViewById(R.id.extra_info_box)
+        isDone = view.findViewById(R.id.is_done)
 
 
 
         return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        extraInfoBox.setOnClickListener{
+            if (extraInfoBox.isEnabled){
+                extraInfo.visibility = View.VISIBLE
+            }
+        }
+
     }
 
 }
